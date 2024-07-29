4a6edd81308baec0380ba5527e7f601b417ce19c
ramkshukla <ramkumarshukla00000@gmail.com>
2024-07-29 11:28:16 +0530

    univerity api added

--- src/main/java/com/example/springboot/controller/oneToMany/unidirectional/UniversityController.java
+++ src/main/java/com/example/springboot/controller/oneToMany/unidirectional/UniversityController.java
@@ -1,9 +1,23 @@
 package com.example.springboot.controller.oneToMany.unidirectional;
 
+import com.example.springboot.entity.oneToMany.unidirectional.University;
+import com.example.springboot.service.oneToMany.unidirectional.UniversityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping(path = "/university")
 public class UniversityController {
+    private final UniversityService universityService;
+
+    public UniversityController(UniversityService universityService) {
+        this.universityService = universityService;
+    }
+
+    @GetMapping
+    public Iterable<University> getUniversities() {
+        return universityService.findAllUniversities();
+    }
 }
--- src/main/java/com/example/springboot/service/oneToMany/unidirectional/UniversityService.java
+++ src/main/java/com/example/springboot/service/oneToMany/unidirectional/UniversityService.java
@@ -1,7 +1,20 @@
 package com.example.springboot.service.oneToMany.unidirectional;
 
+import com.example.springboot.entity.oneToMany.unidirectional.University;
+import com.example.springboot.repository.oneToMany.unidirectional.UniversityRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UniversityService {
+    private final UniversityRepository universityRepository;
+
+    UniversityService(UniversityRepository universityRepository) {
+        this.universityRepository = universityRepository;
+    }
+
+    public Iterable<University> findAllUniversities() {
+        return universityRepository.findAll();
+    }
 }
