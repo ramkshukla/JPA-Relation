a3e031fe5392324bea13a924da3f8828bb3b0d51
ramkshukla <ramkumarshukla00000@gmail.com>
2024-07-30 12:22:20 +0530

    added Log system to app

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -2,17 +2,25 @@ package com.example.springboot;
 
 import com.example.springboot.mail.MailProperties;
 import com.example.springboot.mail.MailService;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+
 @SpringBootApplication
 @EnableConfigurationProperties(MailProperties.class)
 public class SpringbootApplication {
 
+	private final static Logger logger = LogManager.getLogger(SpringbootApplication.class);
+
 	public static void main(String[] args) {
-	ApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
+
+		logger.info("Spring Boot Log is able to display");
+
+		ApplicationContext context = SpringApplication.run(SpringbootApplication.class, args);
 		MailService mailService = context.getBean(MailService.class);
 		mailService.print();
 		// By code
