e88b818c5af7f4c0737ca1cbab47fa1071549e51
ramkshukla <ramkumarshukla00000@gmail.com>
2024-07-30 10:27:24 +0530

    set banner false

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.example.springboot;
 
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,14 @@ public class SpringbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+
+		// By code
+		// By application.properties setting
+
+//		SpringApplication springApplication = new SpringApplication(SpringbootApplication.class);
+//		springApplication.setBannerMode(Banner.Mode.OFF);
+//		springApplication.run(args);
+
 	}
 
 }
